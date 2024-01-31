5b4e7214070ea3fc8983fd6bb4d9324f866d5c39
glznn <gsm004@ucsd.edu>
2024-01-31 12:52:07 -0800

    first committ

---
+++ src/main/java/cerf_project/App.java
@@ -0,0 +1,64 @@
+package cerf_project;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+
+    private static Cell event;
+    private static Workbook wbCopy;
+    private static OutputStream fileOut;
+
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        System.out.println("Hello World!");
+        GUI gui = new GUI();
+        /*
+         * Workbook wb = new XSSFWorkbook();
+         * Sheet sh = wb.createSheet("hi");
+         * try (OutputStream fileOut = new FileOutputStream("workbook.xlsx")) {
+         * wb.write(fileOut);
+         * }
+         * wb.close();
+         */
+
+        // Creates a copy of the CERF template
+        Workbook wb = new XSSFWorkbook("CERFtemplate.xlsx");
+        fileOut = new FileOutputStream("workbook.xlsx");
+        wb.write(fileOut);
+        wb.close();
+
+        Workbook wbCopy = new XSSFWorkbook("workbook.xlsx");
+        Sheet eventInfo = wbCopy.getSheetAt(2);
+        Row row = eventInfo.getRow(23);
+        event = row.getCell(1);
+        System.out.println("At this cell " + event.getStringCellValue());
+        event.setCellValue("testing!");
+
+        wbCopy.write(fileOut);
+        System.out.println("does it reach this");
+    }
+
+    public static boolean changeEvent(String nameEvent) {
+        event.setCellValue(nameEvent);
+        return true;
+    }
+
+    public static boolean saveWb() throws IOException {
+        wbCopy.write(fileOut);
+        return true;
+    }
+
+}
---
+++ src/main/java/cerf_project/GUI.java
@@ -0,0 +1,62 @@
+package cerf_project;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class GUI implements ActionListener {
+
+    private JFrame frame;
+    private JPanel panel;
+    private JTextField cerfTitle;
+
+    public GUI() {
+        frame = new JFrame();
+        panel = new JPanel();
+
+        panel.setLayout(null);
+
+        JLabel label = new JLabel("Enter the event name: ");
+        label.setBounds(20, 20, 250, 25);
+        panel.add(label);
+
+        cerfTitle = new JTextField(20);
+        cerfTitle.setBounds(150, 20, 165, 25);
+        cerfTitle.addActionListener(this);
+        panel.add(cerfTitle);
+
+        frame.setSize(500, 500);
+        frame.add(panel, BorderLayout.CENTER);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("CERF Creation Tool");
+        // frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new GUI();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String text = cerfTitle.getText();
+        App.changeEvent(text);
+        System.out.println("changed");
+        try {
+            App.saveWb();
+        } catch (IOException e1) {
+            // TODO Auto-generated catch block
+            System.out.println("lol");
+            e1.printStackTrace();
+        }
+    }
+}
